3332b7233908cf828b2079a015e88d9a522b2e6f
David Bordeleau <davidbordeleau@hotmail.com>
2019-03-26 17:47:08 +0100

    more options

--- easy/reverse-int.js
+++ easy/reverse-int.js
@@ -1,9 +1,22 @@
+// function reverseInt(n) {
+//   if (n >= 0) {
+//    return Number(n.toString().split("").reverse().join(""));
+//   }
+//   num = n.toString().split("").reverse();
+//   num.unshift("-");
+//   num.pop();
+//   return Number(num.join(""));
+// }
+
+// Or for only positives:
+
 function reverseInt(n) {
-  if (n >= 0) {
-   return Number(n.toString().split("").reverse().join(""));
-  }
-  num = n.toString().split("").reverse();
-  num.unshift("-");
-  num.pop();
-  return Number(num.join(""));
+  const num = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+  return parseInt(num * Math.sign(n));
 }
+
+console.log(reverseInt(32));
