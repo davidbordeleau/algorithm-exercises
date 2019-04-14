a68572f64e4188dae3db130f049fcb0b2c53fbc7
David Bordeleau <davidbordeleau@hotmail.com>
2019-04-14 11:41:37 +0100

    add sort method practice

---
+++ hard/sorting.js
@@ -0,0 +1,12 @@
+function Sort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j+1]) {
+        const lesser = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  console.log(arr);
+}
