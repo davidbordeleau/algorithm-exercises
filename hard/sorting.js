2ba60f0983549738ced521c5e4dabc486ffc5130
David Bordeleau <davidbordeleau@hotmail.com>
2019-04-15 15:34:39 +0200

    add some more sort method for practice

--- hard/sorting.js
+++ hard/sorting.js
@@ -1,4 +1,4 @@
-function Sort(arr) {
+function sort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < (arr.length - i - 1); j++) {
       if (arr[j] > arr[j+1]) {
@@ -8,5 +8,23 @@ function Sort(arr) {
       }
     }
   }
-  console.log(arr);
+  return arr;
+}
+
+function sort2(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] < arr[minIndex]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
