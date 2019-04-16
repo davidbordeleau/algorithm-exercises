501dba86206e27dcdc3ddf8960b29fcee0129d51
David Bordeleau <davidbordeleau@hotmail.com>
2019-04-16 18:24:25 +0200

    add some more recursive method to reverse-string

--- easy/reverse-string.js
+++ easy/reverse-string.js
@@ -6,3 +6,11 @@ function reverse(str) {
  }
  return reversed
 }
+
+function reverseRecursive (str) {
+  if (str === "") {
+    return "";
+  } else {
+    return reverseRecursive(str.substr(1)) + str.charAt(0);
+  }
+}
