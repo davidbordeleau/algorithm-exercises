0b0bf91b6283951a107ab4673c17b2f4768d9bf1
David Bordeleau <davidbordeleau@hotmail.com>
2019-04-16 13:41:12 +0200

    add graph data structure

---
+++ data-structure/graph.js
@@ -0,0 +1,17 @@
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
+  addVertex(node)  {
+    this.adjacentList[node] = [];
+    this.numberOfNodes ++;
+  }
+  addEdge(node1, node2) {
+    //undirected Graph
+    this.adjacentList[node1] += node2;
+    this.adjacentList[node2] += node1;
+    console.log(this.adjacentList);
+
+  }
+}
