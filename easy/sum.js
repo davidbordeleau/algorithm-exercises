a5053f9843d976c0594426f05bd8a04a3fbcabd9
David Bordeleau <davidbordeleau@hotmail.com>
2019-04-19 16:47:42 +0200

    add some sum method O(n) and O(n^2)

---
+++ easy/sum.js
@@ -0,0 +1,21 @@
+function sum(nums, target) {
+  for (i = 0; i < nums.length; i ++) {
+    for (j = 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target && i !== j) {
+        return [i, j]
+      }
+    }
+  }
+};
+
+// O(n^2) and O(1)
+
+function sum2(nums, target) {
+  for (i = 0; i < nums.length; i ++) {
+    if (nums.includes(target - nums[i]) && i !== nums.indexOf(target - nums[i])) {
+      return [i, nums.indexOf(target - nums[i])];
+    }
+  }
+}
+
+// O(n) and O(1)
