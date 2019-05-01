415ee09423357031c2c72dd4e93b321d21fda128
David Bordeleau <davidbordeleau@hotmail.com>
2019-05-01 16:10:15 +0200

    add remove_element ruby method

--- Ruby-exercises/remove_duplicates.rb
+++ Ruby-exercises/remove_duplicates.rb
@@ -4,3 +4,17 @@ def remove_duplicates(nums)
   nums.length.times { |i| nums[i] = nil if nums[i] == nums[i + 1] }
   nums.delete(nil)
 end
+
+# or remove an element
+
+def remove_element(nums, val)
+  nums.delete(val)
+  nums.length
+end
+
+# a bit faster:
+
+def remove_element(nums, val)
+  nums.reject! { |num| num == val }
+  nums.length
+end
